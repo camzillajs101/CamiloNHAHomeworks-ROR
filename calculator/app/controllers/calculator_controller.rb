f8ed692f9a6ae78d7ac47031820abc75635486db
camzillajs101 <camilomason0@gmail.com>
2018-03-04 19:40:15 -0500

    Cleaning code to work with only one view

--- calculator/app/controllers/calculator_controller.rb
+++ calculator/app/controllers/calculator_controller.rb
@@ -1,9 +1,5 @@
 class CalculatorController < ApplicationController
   def main
-  end
-
-  def calculate
     @answer = params[:a].to_i + params[:b].to_i
-    render plain: @answer
   end
 end
--- calculator/config/routes.rb
+++ calculator/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/calculator", to: "calculator#main"
-  post "/calculate", to: "calculator#calculate"
+  post "/calculate", to: "calculator#main"
+
+
   get "/calculate", to: redirect("/calculator")
   get "/", to: redirect("/calculator")
 end
