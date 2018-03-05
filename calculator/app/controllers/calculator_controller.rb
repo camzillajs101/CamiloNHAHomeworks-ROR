13b66292c8c9c650de6c875883685037fb34bbf8
camzillajs101 <camilomason0@gmail.com>
2018-03-04 20:01:35 -0500

    Adding dropdown list for operations (+,-,*,/,**)

--- calculator/app/controllers/calculator_controller.rb
+++ calculator/app/controllers/calculator_controller.rb
@@ -1,5 +1,16 @@
 class CalculatorController < ApplicationController
   def main
-    @answer = params[:a].to_i + params[:b].to_i
+    case params[:dropdown]
+    when "plus"
+      @answer = params[:a].to_i + params[:b].to_i
+    when "minus"
+      @answer = params[:a].to_i - params[:b].to_i
+    when "multiply"
+      @answer = params[:a].to_i * params[:b].to_i
+    when "divide"
+      @answer = params[:a].to_i / params[:b].to_i
+    when "exponent"
+      @answer = params[:a].to_i ** params[:b].to_i
+    end
   end
 end
