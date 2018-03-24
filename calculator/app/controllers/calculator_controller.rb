03e4330a731c571737a963e7ccc7177866c83153
camzillajs101 <camilomason0@gmail.com>
2018-03-24 07:41:20 -0400

    Added and finished animals.

--- calculator/app/controllers/calculator_controller.rb
+++ calculator/app/controllers/calculator_controller.rb
@@ -1,7 +1,7 @@
 class CalculatorController < ApplicationController
   def main
     if params[:a] != nil
-      @answer = params[:a].to_f.send(params[:dropdown], params[:b].to_f).round(5)
+      @answer = params[:a].to_f.send(params[:dropdown], params[:b].to_f)
     end
   end
 end
